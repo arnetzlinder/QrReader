e978ddf08af53a7cbd53fb27fc6fa977687294c0
Sussie Arnetz Linder <smslinder@gmail.com>
2023-10-20 13:43:11 +0200

    First commit

---
+++ TestQR-läsare/src/components/QrReader.vue
@@ -0,0 +1,13 @@
+
+
+<script setup lang="ts">
+import { QrcodeStream } from 'vue-qrcode-reader'
+    const onDetect = (result: any) => {
+        console.log(result)
+    }
+
+</script>
+
+<template>
+    <qrcode-stream @detect="onDetect">Scanna QR-kod här</qrcode-stream>
+</template>
